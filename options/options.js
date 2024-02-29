0fdf772a5c1cf80a8e9575080720f5048d7ce24e
Crowdbotics <team@crowdbotics.com>
2024-02-29 22:27:43 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "noisy-silence-46732",
+  url: "https://noisy-silence-46732.botics.co",
+  api: "https://noisy-silence-46732.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
